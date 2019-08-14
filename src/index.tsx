fffd202b1fa72cc0954a0554dabe0f4274cef0b8
Kacper Biedka <kacper.biedka.00@gmail.com>
2019-08-14 17:03:52 +0200

    Set up redux provider with a store and chrome extension dev tools

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { reducer } from "./reducers/index";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducer,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
+
 serviceWorker.unregister();
