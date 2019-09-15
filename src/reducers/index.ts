671073e34a7ce997119e64cc8115c4a237052e06
Kacper Biedka <kacper.biedka.00@gmail.com>
2019-09-15 18:58:30 +0200

    Updated the testing environment and added redux-thunk

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,10 +1,28 @@
-const initialState = {
-  sampleValue: "sample :)"
+export interface reduxState {
+  temperature: Object;
+}
+
+const initialState: reduxState = {
+  temperature: {}
 };
 
-export const reducer = (state = initialState, action: any) => {
+const getTemperature = (state: reduxState, action: any) => {
+  const temp = action.temperature;
+  return {
+    ...state,
+    temperature: {
+      ...temp
+    }
+  };
+};
+
+const rootReducer = (state = initialState, action: any) => {
   switch (action.type) {
+    case "GET_TEMPERATURE":
+      return getTemperature(state, action);
     default:
       return state;
   }
 };
+
+export default rootReducer;
